3486fd891e4a09480a0557d55347147ac01e6af9
Charlie Mordant <cmordant1@gmail.com>
2021-05-17 19:55:46 +0200

    [travis skip] some variables

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,13 +1,16 @@
 """Role testing files using testinfra."""
 
 
-def test_vagrant_machine_is_fully_up(host):
-    command = r"""cat /home/vagrant/myvmerr.log | egrep -c 'SSH\sis\sready'"""
-    cmd = host.run(command)
-    assert '1' in cmd.stdout
-
-
 def test_vagrant_machine_is_running(host):
     command = r"""vagrant status | egrep -c 'myvm\s*running\s\(libvirt\)'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
+
+
+def test_ip_forwarding_is_enabled(host):
+    command = r"""
+    sysctl net.ipv4.conf.eth0.forwarding | \
+    cut -d " " -f 3"""
+    with host.sudo():
+        cmd = host.run(command)
+        assert '1' in cmd.stdout
