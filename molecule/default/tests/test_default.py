339262d0817edf1d5f1590de4425c2cb249ae834
Charlie Mordant <cmordant1@gmail.com>
2023-08-17 17:11:46 +0200

    fix tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,7 +2,7 @@
 
 
 def test_vagrant_machine_is_running(host):
-    command = r"""vagrant status | egrep -c 'myvm\s*running\s\(libvirt\)'"""
+    command = r"""vagrant status | grep -Ec 'myvm\s*running\s\(libvirt\)'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
